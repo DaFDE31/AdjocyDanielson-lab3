435d640076c3e2123f8992e00f1f4135616e852a
DaFDE31 <adjocydanielson@gmail.com>
2023-11-20 05:01:51 +0000

    hi

--- lab3.c
+++ lab3.c
@@ -5,10 +5,12 @@
 
 extern int** sudoku_board;
 int* worker_validation;
+int** board = NULL;
+
 
 int** read_board_from_file(char* filename){
     FILE *fp = fopen(filename, "r");
-    int** board = NULL;
+    
 
     // replace this comment with your code
     board = (int**)malloc(9*sizeof(int*));
@@ -16,7 +18,10 @@ int** read_board_from_file(char* filename){
     {
         board[col] = (int*)malloc(9*sizeof(int));
     }
-    if(fp == NULL) return;
+    if(fp == NULL){
+        fprintf(stderr, "No file.");
+        return;
+        }
 
     int row = 0;
     while(row < 9){
@@ -38,16 +43,50 @@ int is_board_valid(){
     param_struct* parameter;
     
     // replace this comment with your code
+    tid = (pthread_t*) malloc(sizeof(int*) * NUM_OF_THREADS);
+    parameter = (param_struct*) malloc(sizeof(param_struct) * NUM_OF_THREADS);
+
+    for (int spot = 0; spot < ROW_SIZE; spot++){
+        parameter[spot].starting_row = spot;
+        parameter[spot].starting_col = 0;
+        parameter[spot].ending_col = COL_SIZE-1;
+        parameter[spot].ending_row = spot;
+        int check [9];
+        int index = 0;
+        for (int row = parameter[spot].starting_row; row < parameter[spot].ending_row; row++){
+            for (int col = parameter[spot].starting_col; col < parameter[spot].ending_row; col++){
+                check[index] = sudoku_board[row][col];
+            }
+        }
+        pthread_create(&(tid[spot]), &attr, board_piece(check), &check);
+    }
+
+    for (int spot = 0; spot < COL_SIZE; spot++){
+        parameter[spot].starting_row = 0;
+        parameter[spot].starting_col = spot;
+        parameter[spot].ending_col = spot;
+        parameter[spot].ending_row = ROW_SIZE-1;
+        int check [9];
+        int index = 0;
+        for (int row = parameter[spot].starting_row; row < parameter[spot].ending_row; row++){
+            for (int col = parameter[spot].starting_col; col < parameter[spot].ending_row; col++){
+                check[index] = sudoku_board[row][col];
+            }
+        }
+        pthread_create(&(tid[spot]), &attr, board_piece(check), &check);
+    }
+
+
     
 }
 
 int board_piece(int* row){
-    int* checker[] ={0, 0,0,0,0,0,0,0,0}; 
+    int checker[] ={0,0,0,0,0,0,0,0,0}; 
     for (int value =0; value < 9; value++){
         checker[row[value]-1] = 1;
     }
     for (int value =0; value < 9; value++){
-        if(checker[value] == 0){
+        if(checker[value] != 1){
             return 0;
         }
     }
