059709ad0e09647c81c6f0e8c0569b879f5679c3
DaFDE31 <adjocydanielson@gmail.com>
2023-12-01 02:43:47 +0000

    Cleaned up and done

--- lab3.c
+++ lab3.c
@@ -4,11 +4,8 @@
 #include "lab3.h"
 
 extern int** sudoku_board;
-//int* worker_validation;
 
 int* valid;
-
-int**board;
 
 int** read_board_from_file(char* filename){
     FILE *fp = fopen(filename, "r");
@@ -48,24 +45,16 @@ void* board_piece(void* piece){
 
     int checker[9] ={0,0,0,0,0,0,0,0,0};
     for (int row = sr; row <= er; row++){
-        //printf("Row #%d\n",row);
-
         for (int col = sc; col <= ec; col++){
-            //printf("Row #%d Col#%d\n",row, col);
-
-            //printf("%d",board[row][col]);
             checker[ sudoku_board[row][col] -1] = 1;
         }
     } 
 
     for (int value =0; value < 9; value++){
-        //printf("%d ", checker[value]); BRING THIS AND THE NEXT BACK
         if(checker[value] != 1){
             valid[spot] = 0;
         }
     }
-    printf("SPOT: %d VALID: %d Starting row:%d Ending row:%d Starting column: %d Ending column: %d\n",spot, valid[spot], sr,er,sc,ec);
-    //printf("%d ", valid[spot]);
 }
 
 
@@ -73,27 +62,16 @@ int is_board_valid(){
     pthread_t* tid;  /* the thread identifiers */
     pthread_attr_t attr;
     param_struct* parameter;
-    // replace this comment with your code
     tid = (pthread_t*) malloc(sizeof(int*) * NUM_OF_THREADS);   
     parameter = (param_struct*) malloc(sizeof(param_struct) * NUM_OF_THREADS);
     valid = (int*)malloc(27*sizeof(int));
-    int identity = 0;// id variable, while spot is used to denote rows and such.
+    int identity = 0;
     for (int spot = 0; spot < ROW_SIZE; spot++){
         parameter[identity].id = identity;
         parameter[identity].starting_row = spot;
         parameter[identity].starting_col = 0;
         parameter[identity].ending_col = COL_SIZE-1;
         parameter[identity].ending_row = spot;
-        /*int check [9];
-        int index = 0;
-        //for (int row = parameter[spot].starting_row; row <= parameter[spot].ending_row; row++){
-            for (int col = parameter[spot].starting_col; col < parameter[spot].ending_row; col++){
-                check[index] = sudoku_board[spot][col];
-                index++;
-            }
-        //}
-        */
-        printf("IDENTITY: %d, ID %d\n", identity, parameter[identity].id);
         pthread_create(&(tid[identity]), NULL, board_piece, &parameter[identity]);
         identity++;
     }
@@ -104,16 +82,6 @@ int is_board_valid(){
         parameter[identity].starting_col = spot;
         parameter[identity].ending_col = spot;
         parameter[identity].ending_row = ROW_SIZE-1;
-        /*int check [9];
-        int index = 0;
-        for (int row = parameter[spot].starting_row; row < parameter[spot].ending_row; row++){
-            //for (int col = parameter[spot].starting_col; col <= parameter[spot].ending_row; col++){
-                check[index] = sudoku_board[row][spot];
-                index++;
-            //}
-        }
-        */
-        printf("IDENTITY: %d, ID %d\n", identity, parameter[identity].id);
         pthread_create(&(tid[identity]), NULL, board_piece, &parameter[identity]);
         identity++;
     }
@@ -126,16 +94,6 @@ int is_board_valid(){
         parameter[identity].starting_col = vert;
         parameter[identity].ending_col = vert+2;
         parameter[identity].ending_row = horz+2;
-        /*int check [9];
-        int index = 0;
-        for (int row = parameter[spot].starting_row; row < parameter[spot].ending_row; row++){
-            for (int col = parameter[spot].starting_col; col < parameter[spot].ending_row; col++){
-                check[index] = sudoku_board[row][col];
-                index++;
-            }
-        }
-        */
-        printf("IDENTITY: %d, ID %d\n", identity, parameter[identity].id);
         pthread_create(&(tid[identity]), NULL, board_piece, &parameter[identity]);
         identity++;
         if(vert == 6){
@@ -147,12 +105,10 @@ int is_board_valid(){
         }
     }
     for(int i =0; i< 27; i++){
-        //printf("%s", (char*)tid[i]);
         pthread_join(tid[i], NULL);
     }
 
     for (int v = 0; v < 27; v++){
-        printf("VALIDATOR: %d ", valid[v]);// BRING THIS BACK
         if (valid[v] != 1){  
             return 0; 
         }
@@ -161,35 +117,3 @@ int is_board_valid(){
     free(tid);
    return 1;
 }
-/*
-int main(int argc, char *argv[]){
-    
-
-
-
-    board = read_board_from_file(argv[1]);
-    for(int i = 0; i<9; i++ ){
-        for (int e = 0; e<9; e++){
-            printf("%d",board[i][e]);
-        }
-        printf("\n");
-    }
-    
-    
-   
-    if (is_board_valid()){
-        printf("The board is valid.\n");
-        printf("RAHHHHHHHH: 1");
-    }
-    else{
-        printf("The board is not valid.\n");
-        printf("RAHHHHHHHH: 0");
-    }
-    
-    for(int row = 0; row < ROW_SIZE; row++){
-        free(board[row]);
-    }
-    free(board);
-    return 0;
-}
-*/
